ed2895969f7325b502b67b24fb1260d9f78b1888
Bartosz Drozd <bartosz.drozd94@gmail.com>
2020-11-29 15:05:32 +0100

    connection and parse initialization

---
+++ hack.py
@@ -0,0 +1,13 @@
+# write your code here
+import socket
+import argparse
+
+client_connection = socket.socket()
+parser = argparse.ArgumentParse()
+parser.add_argument()
+parser.add_argument()
+parser.add_argument()
+args = parser.parse.args()
+
+
+
