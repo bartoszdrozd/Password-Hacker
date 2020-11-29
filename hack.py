411c027c51de59a64de608999c86a756e0912506
Bartosz Drozd <bartosz.drozd94@gmail.com>
2020-11-29 22:57:39 +0100

    establish connection and get response from server

--- hack.py
+++ hack.py
@@ -1,4 +1,3 @@
-# write your code here
 import socket
 import argparse
 
@@ -8,7 +7,6 @@ parser.add_argument("hostname")
 parser.add_argument("port", type=int)
 parser.add_argument("message")
 args = parser.parse_args()
-#print(args)
 
 address = (args.hostname, args.port)
 client_socket.connect(address)
@@ -20,4 +18,5 @@ client_socket.send(data)
 response = client_socket.recv(1024)
 response = response.decode()
 print(response)
+
 client_socket.close()
