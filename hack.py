849c3d12c0ed452ae43c4a72c99cc9ea6d6074b3
Bartosz Drozd <bartosz.drozd94@gmail.com>
2020-11-29 22:50:50 +0100

    connection and parse initialization

--- hack.py
+++ hack.py
@@ -2,12 +2,17 @@
 import socket
 import argparse
 
-client_connection = socket.socket()
-parser = argparse.ArgumentParse()
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
-args = parser.parse.args()
+client_socket = socket.socket()
+parser = argparse.ArgumentParser()
+parser.add_argument("hostname")
+parser.add_argument("port")
+parser.add_argument("message")
+args = parser.parse_args()
+print(args)
 
+address = (args.hostname, args.port,)
+client_socket.connect(address)
 
-
+data = args.message
+data = data.encode()
+client_socket.send(data)
